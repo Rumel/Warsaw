3319dc8c43e1099c3be902674ec7b47f7d9ea19c
Dalton Dick <daltondick2010@gmail.com>
2012-05-24 12:03:29 -0500

    Decided to split the program into two files.

--- handbrakeBatch.py
+++ handbrakeBatch.py
@@ -1,8 +1,7 @@
 #Written and tested with python 2.7.3
 
 import os
-import time
-import datetime
+import handbrakeBatchFuntions
 
 #This is where the script should look for files
 inputDir = ""
@@ -22,105 +21,6 @@ newExt = ""
 fileFormats = ["mpg", "mkv", "avi", "wmv", "mp4", "flv", "mt2s", "mpeg", "mov", "f4v" ]
 #Location of HandbrakeCLI.exe
 handBrake = ""
-
-def isVideoFile(ext):
-	for f in fileFormats:
-		if(str.lower(ext) == f):
-			return True
-	return False
-	
-def isValidDir(directory):
-	if(os.path.exists(directory) and os.path.isdir(directory)):
-		return True
-	else:
-		return False
-		
-def makeDirs(directory):
-	os.makedirs(directory)
-	return
-
-#Formats the directory string to how I want it (to have a trailing '\')
-def trailingSlash(directory):
-	directory = directory.replace("\n", "")
-	if(directory[len(directory) - 1] != "\\"):
-		return directory + "\\"
-	else:
-		return directory
-	
-#This function provides for more consistent formatting of the logfile	
-def addZero(num):
-	if(len(str(num)) == 1):
-		return "0" + str(num)
-	else:
-		return str(num)
-
-def timeString():
-	t = time.localtime()
-	month = addZero(t.tm_mon)
-	day = addZero(t.tm_mday)
-	year = addZero(t.tm_year)
-	hour = ""
-	
-	if(t.tm_hour > 12):
-		hour = addZero(t.tm_hour - 12)
-	elif(t.tm_hour == 0):
-		hour = "12"
-	else:
-		hour = addZero(t.tm_hour)
-		
-	minutes = addZero(t.tm_min)
-	seconds = addZero(t.tm_sec)
-	ampm = ""
-	
-	if(t.tm_hour < 12):
-		ampm = "AM"
-	else:
-		ampm = "PM"
-	
-	#this is unecessary but provides an easier to read format
-	# for the return string
-	h = "-"
-	s = " "
-	c = ":"
-		
-	return month + h + day + h + year + s + hour + c + minutes + c + seconds + s + ampm
-
-def convertTime(seconds):
- 	hours = int(seconds / 3600)
-	seconds = seconds - hours * 3600
-	minutes = int(seconds / 60)
-	seconds = seconds - minutes * 60
-	seconds = int(seconds % 60)
-	#Old
-	#s = addZero(hours) + " hours " + addZero(minutes) + " mins " + addZero(seconds) + " secs"
-	s = addZero(hours) + "." + addZero(minutes) + "." + addZero(seconds) 
-	return s
-	
-def humanSize(bytes):
-	gb = 1073741824.0
-	mb = 1048576.0
-	kb = 1024.0
-	if(bytes > gb):
-		left = int(bytes/gb)
-		bytes = bytes - left * gb
-		percent = round((bytes/gb), 2)
-		return "%6s GB" % str(percent + left)
-	elif(bytes > mb):
-		left = int(bytes/mb)
-		bytes = bytes - left * mb
-		percent = round((bytes/mb), 2)
-		return "%6s MB" % str(percent + left)
-	elif(bytes > kb):
-		left = int(bytes/kb)
-		bytes = bytes - left * kb
-		percent = round((bytes/kb), 1)
-		return "%6s KB" % str(percent + left)
-	else:
-		return "%6s B" % str(bytes)
-
-###############################
-##Start of program logic here##
-###############################
 		
 if(os.path.exists(inputDir) == False):
 	print "Input directory doesn't exist"
