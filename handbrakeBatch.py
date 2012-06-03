4eeb009426424127533e0e0dd9c79c0c1ab9b393
Dalton Dick <daltondick2010@gmail.com>
2012-06-03 01:29:42 -0500

    Fixing the program

--- handbrakeBatch.py
+++ handbrakeBatch.py
@@ -1,7 +1,6 @@
 #Written and tested with python 2.7.3
-
 import os
-import handbrakeBatchFuntions
+import time
 
 #This is where the script should look for files
 inputDir = ""
@@ -10,7 +9,7 @@ outputDir = ""
 #Specify the Handbrake preset here
 #Example:
 # '--preset "Android High"'
-preset = ""
+preset = ''
 #Specify where you want the logfile
 # use the \\ syntax
 logFile = ""
@@ -20,7 +19,103 @@ newExt = ""
 #List of video formats. This list is pretty small at the moment.
 fileFormats = ["mpg", "mkv", "avi", "wmv", "mp4", "flv", "mt2s", "mpeg", "mov", "f4v" ]
 #Location of HandbrakeCLI.exe
-handBrake = ""
+#Not currently working yet, you will need to add Handbrake to your path
+handBrake = 'HandBrakeCLI.exe'
+
+def isVideoFile(ext, fileformats):
+	for f in fileFormats:
+		if(str.lower(ext) == f):
+			return True
+	return False
+	
+def isValidDir(directory):
+	if(os.path.exists(directory) and os.path.isdir(directory)):
+		return True
+	else:
+		return False
+		
+def makeDirs(directory):
+	os.makedirs(directory)
+	return
+
+#Formats the directory string to how I want it (to have a trailing '\')
+def trailingSlash(directory):
+	directory = directory.replace("\n", "")
+	if(directory[len(directory) - 1] != "\\"):
+		return directory + "\\"
+	else:
+		return directory
+	
+#This function provides for more consistent formatting of the logfile	
+def addZero(num):
+	if(len(str(num)) == 1):
+		return "0" + str(num)
+	else:
+		return str(num)
+
+def timeString():
+	t = time.localtime()
+	month = addZero(t.tm_mon)
+	day = addZero(t.tm_mday)
+	year = addZero(t.tm_year)
+	hour = ""
+	
+	if(t.tm_hour > 12):
+		hour = addZero(t.tm_hour - 12)
+	elif(t.tm_hour == 0):
+		hour = "12"
+	else:
+		hour = addZero(t.tm_hour)
+		
+	minutes = addZero(t.tm_min)
+	seconds = addZero(t.tm_sec)
+	ampm = ""
+	
+	if(t.tm_hour < 12):
+		ampm = "AM"
+	else:
+		ampm = "PM"
+	
+	#this is unecessary but provides an easier to read format
+	# for the return string
+	h = "-"
+	s = " "
+	c = ":"
+		
+	return month + h + day + h + year + s + hour + c + minutes + c + seconds + s + ampm
+
+def convertTime(seconds):
+ 	hours = int(seconds / 3600)
+	seconds = seconds - hours * 3600
+	minutes = int(seconds / 60)
+	seconds = seconds - minutes * 60
+	seconds = int(seconds % 60)
+	#Old
+	#s = addZero(hours) + " hours " + addZero(minutes) + " mins " + addZero(seconds) + " secs"
+	s = addZero(hours) + "." + addZero(minutes) + "." + addZero(seconds) 
+	return s
+	
+def humanSize(bytes):
+	gb = 1073741824.0
+	mb = 1048576.0
+	kb = 1024.0
+	if(bytes > gb):
+		left = int(bytes/gb)
+		bytes = bytes - left * gb
+		percent = round((bytes/gb), 2)
+		return "%6s GB" % str(percent + left)
+	elif(bytes > mb):
+		left = int(bytes/mb)
+		bytes = bytes - left * mb
+		percent = round((bytes/mb), 2)
+		return "%6s MB" % str(percent + left)
+	elif(bytes > kb):
+		left = int(bytes/kb)
+		bytes = bytes - left * kb
+		percent = round((bytes/kb), 1)
+		return "%6s KB" % str(percent + left)
+	else:
+		return "%6s B" % str(bytes)
 		
 if(os.path.exists(inputDir) == False):
 	print "Input directory doesn't exist"
@@ -39,7 +134,7 @@ while(True):
 		splitName = f.split('.')
 		length = len(splitName) - 1
 		oldExt = splitName[length]
-		if(isVideoFile(oldExt)):
+		if(isVideoFile(oldExt, fileFormats)):
 			oldFile = ".".join(splitName)
 			splitName[length] = newExt
 			newFile = ".".join(splitName)
